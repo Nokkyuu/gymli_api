462ec8452f898ed1edbb108be19980a5af43a451
Nokkyuu <nils.ayral@alum.uol.de>
2025-05-15 08:09:26 +0200

    adding middleware functionality and allowing gymli web adress

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from app.api import animals
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Animal API")
 
@@ -8,3 +9,12 @@ app.include_router(animals.router)
 @app.get("/")
 def read_root():
     return {"status": "API is running"}
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://icy-ground-0e9ef4303.6.azurestaticapps.net/", "https://gymli.brgmnn.de/"],  # or specify: ["https://your-flutter-web-url"]
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
