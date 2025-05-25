561848b8e3061245da5d01dd18c779338da8a97d
Nokkyuu <nils.ayral@alum.uol.de>
2025-05-25 12:26:32 +0200

    models and api endpoints for gymli

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from app.db import Base, engine
-from app.models import Animal
+from app.models import Animal, Exercise, TrainingSet, WorkoutUnit, Workout
 
 #run only once to create table
 Base.metadata.create_all(bind=engine)
